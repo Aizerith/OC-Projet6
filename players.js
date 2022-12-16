3968a3c5ac8f4a49e5b7513969de23cfcbbd8032
Aizerith <101933140+Aizerith@users.noreply.github.com>
2022-12-16 12:43:25 +0100

    Add files via upload
    player.js moveplayer arrowmoveplayer

--- players.js
+++ players.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor(name, imgSrc){
+    constructor(name, imgSrc) {
         this.name = name;
         this.hp = 100;
         this.weapon = new Weapon("couteau", 10, 'knife.png');
@@ -12,59 +12,57 @@ class Player {
         this.panel;
     }
 
-    getCloseCells(x, y, mapX, mapY){
+    getCloseCells(x, y, mapX, mapY) {
 
         const allCells = document.querySelector('table');
         console.log(allCells, x, y);
         const CloseCells = [];
-        (y > 0) ? CloseCells.push(allCells.rows[y-1].cells[x]) : "";
-        (y < mapY-1) ? CloseCells.push(allCells.rows[y+1].cells[x]) : "";
-        (x > 0) ? CloseCells.push(allCells.rows[y].cells[x-1]) : "";
-        (x < mapX-1) ? CloseCells.push(allCells.rows[y].cells[x+1]) : "";
+        (y > 0) ? CloseCells.push(allCells.rows[y - 1].cells[x]): "";
+        (y < mapY - 1) ? CloseCells.push(allCells.rows[y + 1].cells[x]): "";
+        (x > 0) ? CloseCells.push(allCells.rows[y].cells[x - 1]): "";
+        (x < mapX - 1) ? CloseCells.push(allCells.rows[y].cells[x + 1]): "";
         console.log("getclosecells:", CloseCells);
         return (CloseCells)
     }
 
-    getMovableCells(x, y, mapX, mapY){
+    /*===== NOTE MENTOR : il ne doit pas y avoir d'autre joueur dans ces cases =======*/
+
+    getMovableCells(x, y, mapX, mapY) {
         const allCells = document.querySelector('table');
         const movableCells = [];
         let moveX = 1;
         let moveY = 1;
-        for (let i = 0; i < 3; i++)
-        {
-            if (y - moveY >= 0 &&
-                !allCells.rows[y-moveY].cells[x].classList.contains('disabled'))
-            {
-                movableCells.push(allCells.rows[y-moveY].cells[x])
+        for (let i = 0; i < 3; i++) {
+            if (y - moveY >= 0
+                && !allCells.rows[y - moveY].cells[x].classList.contains('disabled')
+                && !allCells.rows[y - moveY].cells[x].classList.contains('has-player')) {
+                movableCells.push(allCells.rows[y - moveY].cells[x])
                 moveY++;
             }
         }
         moveY = 1;
-        for (let i = 0; i < 3; i++)
-        {
-            if (y + moveY < mapY &&
-                !allCells.rows[y+moveY].cells[x].classList.contains('disabled'))
-            {
-                movableCells.push(allCells.rows[y+moveY].cells[x])
+        for (let i = 0; i < 3; i++) {
+            if (y + moveY < mapY
+                && !allCells.rows[y + moveY].cells[x].classList.contains('disabled')
+                && !allCells.rows[y + moveY].cells[x].classList.contains('has-player')) {
+                movableCells.push(allCells.rows[y + moveY].cells[x])
                 moveY++;
             }
         }
-        for (let i = 0; i < 3; i++)
-        {
-            if (x - moveX >= 0 &&
-                !allCells.rows[y].cells[x-moveX].classList.contains('disabled'))
-            {
-                movableCells.push(allCells.rows[y].cells[x-moveX])
+        for (let i = 0; i < 3; i++) {
+            if (x - moveX >= 0
+                && !allCells.rows[y].cells[x - moveX].classList.contains('disabled')
+                && !allCells.rows[y].cells[x - moveX].classList.contains('has-player')) {
+                movableCells.push(allCells.rows[y].cells[x - moveX])
                 moveX++;
             }
         }
         moveX = 1;
-        for (let i = 0; i < 3; i++)
-        {
-            if (x + moveX < mapX &&
-                !allCells.rows[y].cells[x+moveX].classList.contains('disabled'))
-            {
-                movableCells.push(allCells.rows[y].cells[x+moveX])
+        for (let i = 0; i < 3; i++) {
+            if (x + moveX < mapX
+                && !allCells.rows[y].cells[x + moveX].classList.contains('disabled')
+                && !allCells.rows[y].cells[x + moveX].classList.contains('has-player')) {
+                movableCells.push(allCells.rows[y].cells[x + moveX])
                 moveX++;
             }
         }
@@ -72,29 +70,29 @@ class Player {
         return movableCells;
     }
 
-    showMovableCells(movableCells){
-        movableCells.forEach(cell=>{
+    showMovableCells(movableCells) {
+        movableCells.forEach(cell => {
             cell.classList.add('moveCells');
         })
     }
 
-    removeMovableCells(movableCells){
-        movableCells.forEach(cell=>{
+    removeMovableCells(movableCells) {
+        movableCells.forEach(cell => {
             cell.classList.remove('moveCells');
         })
     }
 
-    checkWeapon(selectedCell){
+    checkWeapon(selectedCell) {
         if (selectedCell.classList.contains('has-weapon'))
             return true;
         else
             return false;
     }
 
-    getWeapon(newWeapon){
+    getWeapon(newWeapon) {
         let playerWeapon;
-        this.game.weapons.forEach(weapon=>{
-            if(weapon.name == newWeapon){
+        this.game.weapons.forEach(weapon => {
+            if (weapon.name == newWeapon) {
                 console.log("arme trouvé", weapon);
                 playerWeapon = weapon;
             }
@@ -102,7 +100,7 @@ class Player {
         return playerWeapon;
     }
 
-    switchWeapon(startCell, endCell){
+    switchWeapon(startCell, endCell) {
 
         console.log("startcell endcell", startCell, endCell);
         const newWeapon = endCell.querySelector('.has-weapon').getAttribute('data-weapon');
@@ -116,78 +114,34 @@ class Player {
         this.panel.refreshPanel();
     }
 
-    startCombat(){
+    startCombat() {
         console.log("COMBAT!!");
-        if (this.hp <= 0 || this.enemy.hp <= 0){
+        if (this.hp <= 0 || this.enemy.hp <= 0) {
             return;
-        } 
-        if(this.enemy.atkMode) {
+        }
+        if (this.enemy.atkMode) {
             this.enemy.hp = this.enemy.hp - this.weapon.damage;
             this.hp = this.hp - this.enemy.weapon.damage;
-        }
-        else {
+        } else {
             this.enemy.hp = this.enemy.hp - (this.weapon.damage / 2);
         }
     }
 
-    /*
-    movePlayer(x, y, allCells)
-    {
-        allCells.rows[this.curY].cells[this.curX].classList.remove('has-player');
-        allCells.rows[this.curY].cells[this.curX].removeChild(allCells.rows[this.curY].cells[this.curX].firstChild);    
-        
-        const playerElt = this.createPlayerElt();
-        const newCell = allCells.rows[y].cells[x]; 
-        
-        if (newCell.classList.contains("has-player")){
-                    this.startCombat();
-        }
-        if (this.checkWeapon(newCell)){
-            this.switchWeapon(allCells.rows[this.curY].cells[this.curX], newCell);
-        }
-
-        newCell.classList.add("has-player");
-        newCell.appendChild(playerElt);
-        this.curX = x;
-        this.curY = y;
-    }
-    */
-   
-    playerPlay(map, allCells)
-    {
-        if (!this.playerTurn){
+    playerPlay(map, allCells) {
+        if (!this.playerTurn) {
             return
+        } else {
+           // this.movePlayer(allCells, map);
+           this.arrowMove(allCells, map);
         }
-        else {
-            //this.movePlayer(allCells, map);
-            this.arrowMove(allCells, map);
-        }
-        /*
-        const movableCells = this.getMovableCells(this.curX, this.curY, map.nb, map.rows);
-        this.showMovableCells(movableCells);
-        const that = this;
-
-        movableCells.forEach(cell=>{
-            cell.addEventListener('click', function(e){
-                e.preventDefault();
-                console.log(this.cellIndex, this.parentNode.rowIndex);
-                if(that.playerTurn){
-                        that.movePlayer(this.cellIndex, this.parentNode.rowIndex, allCells, map);
-                        that.playerTurn = false;
-                        that.enemy.playerTurn = true;
-                        that.removeMovableCells(movableCells);
-                }
-            });
-        })*/
     }
 
     // vérifie s'il y a des joueurs à coté
-    checkClosePlayer(x = this.curX, y = this.curY, mapX, mapY){
+    checkClosePlayer(x = this.curX, y = this.curY, mapX, mapY) {
         const closeCells = this.getCloseCells(x, y, mapX, mapY);
         let hasWarrior = false;
-        closeCells.forEach(el=>{
-            if(el && el.classList.contains('has-player'))
-            {
+        closeCells.forEach(el => {
+            if (el && el.classList.contains('has-player')) {
                 console.log("test")
                 hasWarrior = true;
             }
@@ -195,31 +149,29 @@ class Player {
         return hasWarrior
     }
 
-    getRandNb(length){
+    getRandNb(length) {
         return Math.floor(Math.random() * length);
     }
 
     // créer l'élément joueur
-    createPlayerElt()
-    {
+    createPlayerElt() {
         const playerElt = document.createElement('span');
         playerElt.classList.add('has-player');
         playerElt.setAttribute("data-player", this.name);
-        // playerElt.innerHTML= `<img src="./src/${this.imgSrc}">`
         playerElt.style.backgroundImage = `url(./src/${this.imgSrc})`;
-        
+
         return playerElt;
     }
 
     // place le joueur sur la map après la création
-    placePlayer(mapX, mapY){
+    placePlayer(mapX, mapY) {
         const allCells = document.querySelectorAll('.allMap');
         let selectedCell = allCells[Math.floor(Math.random() * allCells.length)];
         this.curX = selectedCell.cellIndex;
         this.curY = selectedCell.parentNode.rowIndex;
 
         const playerElt = this.createPlayerElt();
-        
+
         const placePlayerInCell = (cell, player) => {
             cell.classList.add('has-player');
             cell.appendChild(player);
@@ -232,118 +184,190 @@ class Player {
             !selectedCell.classList.contains("disabled")
         ) {
             placePlayerInCell(selectedCell, playerElt);
-        }
-        else {
+        } else {
             this.placePlayer();
         }
     }
 
-    getGame(game){
+    getGame(game) {
         this.game = game;
     }
 
+    /*===== INTERVENTION MENTOR =======*/
     movePlayer(allCells, map) {
-    //1 - récupérer les cases libres 
+        //1 - récupérer les cases libres 
         const movableCells = this.getMovableCells(this.curX, this.curY, map.nb, map.rows);
+        const oldCell = allCells.rows[this.curY].cells[this.curX];//Stocker l'ancienne cellule dans une variable au lieu de répéter à chaque fois
         this.showMovableCells(movableCells);
         const that = this;
 
         movableCells.forEach(cell => {
-        
-        cell.addEventListener('click', function(e) {
-            e.preventDefault();
-            //1 - supprimer la classe moveCells
-            if(!that.playerTurn){
-                return;
-            }
-            that.removeMovableCells(movableCells);
-            //2 Déplacer le joueur dans la case 
-            
-            const newCell = allCells.rows[this.parentNode.rowIndex].cells[this.cellIndex];
 
-            if (newCell.classList.contains("has-player")) {
-                that.startCombat();
-                return;
-            }
+            cell.addEventListener('click', function (e) {
+                e.preventDefault();
+                console.log('Click turn : ', that.playerTurn)
 
-            allCells.rows[that.curY].cells[that.curX].classList.remove('has-player');
-            allCells.rows[that.curY].cells[that.curX].removeChild(allCells.rows[that.curY].cells[that.curX].firstChild);
+                if (!that.playerTurn) {
+                    return;
+                }
+                //Supprimer la classe moveCells
+                that.removeMovableCells(movableCells);
 
-            if (that.checkWeapon(newCell)) {
-                that.switchWeapon(allCells.rows[that.curY].cells[that.curX], newCell);
-            }
-            const playerElt = that.createPlayerElt();
-            newCell.classList.add("has-player");
-            newCell.appendChild(playerElt);
-            that.curX = this.cellIndex;
-            that.curY = this.parentNode.rowIndex;
-            that.playerTurn = false;
-            that.enemy.playerTurn = true;
+                //Définir la nouvelle celleule = inutile c'est cell on vient de cliquer dessus !
+                //const newCell = allCells.rows[this.parentNode.rowIndex].cells[this.cellIndex];
+
+                //Vérifier pour une arme
+                if (that.checkWeapon(cell)) {
+                    that.switchWeapon(oldCell, cell);
+                }
+
+                //Nettoyer ancienne cellule
+                oldCell.classList.remove('has-player');
+                oldCell.removeChild(oldCell.firstChild);
+
+                //Déplacer le joueur dans la case                
+                const playerElt = that.createPlayerElt();
+                cell.classList.add("has-player");
+                cell.appendChild(playerElt);
+                
+                //mettre à jour la position curX et CurY
+                that.curX = cell.cellIndex; //attention pas de this
+                that.curY = cell.parentNode.rowIndex;
+                
+                //mettre à jour la position curX et CurY
+                that.playerTurn = false;
+                that.enemy.playerTurn = true;
+
+
+                //Verifier si joueur proche
+                /*
+               
+                
+                ============    Dans les lignes dessous tu dis : si la case dans laquelle je clique a un autre joueur alors on combat
+                                Ce n'est pas ce qu'on te demande
+                                Il faut vérifier les cases à côté avec qqch dans le genre
+                                const closeCells = that.getCloseCells()
+                                closeCells.forEach(cell=>{ 
+                                    if( cell.classList.contains("warrior") ){
+                                        that.startCombat()
+                                    }
+                                })
+                                
+                if (newCell.classList.contains("has-player")) {
+                    that.startCombat();
+                    return;
+                }
+                
+                            Il ne faut pas qu'une case avec un joueur se trouve dans tes movableCells
+                
+                
+                */
+                
+                // si joueur proche combat
+
+                const closeCells = that.getCloseCells(that.curX, that.curY, map.nb, map.rows);
+                closeCells.forEach(cell=>{ 
+                    if( cell.classList.contains("has-player") ){
+                        that.startCombat();
+                    }
+                })
+            })
         })
-    })
-  }
+    }
 
-  moveFocusedPlayer(event) {
-    const start = document.activeElement;
-	let startX = start.cellIndex;
-	let startY = start.parentNode.rowIndex;
+    moveFocusedPlayer(startCell, endCell, movableCells){
+        if (!this.playerTurn) {
+            return;
+        }
+        //Supprimer la classe moveCells
+        startCell.classList.remove("moveCells");
+        this.removeMovableCells(movableCells);
 
-	const key = event.key;
-	let newX = startX;
-	let newY = startY;
-  
-  console.log(key);
-    if (this.playerTurn){
-	switch(key){
-		case "ArrowUp" :
-    	newY = startY - 1;
-    	break;
-    case "ArrowDown" :
-    	newY = startY + 1;
-    	break;
-    case "ArrowLeft" :
-    	newX = startX - 1;
-    	break;
-    case "ArrowRight" :
-    	newX = startX + 1;
-    	break;
-    case "Enter" :
+        //Vérifier pour une arme
+        if (this.checkWeapon(endCell)) {
+            this.switchWeapon(startCell, endCell);
+        }
+
+        //Nettoyer ancienne cellule
+        startCell.classList.remove('has-player');
+        startCell.removeChild(startCell.firstChild);
+
+        //Déplacer le joueur dans la case                
+        const playerElt = this.createPlayerElt();
+        endCell.classList.add("has-player");
+        endCell.appendChild(playerElt);
+        
+        //mettre à jour la position curX et CurY
+        this.curX = endCell.cellIndex;
+        this.curY = endCell.parentNode.rowIndex;
+        
+        //mettre à jour la position curX et CurY
         this.playerTurn = false;
         this.enemy.playerTurn = true;
-        break;
+
+        const closeCells = this.getCloseCells(this.curX, this.curY, this.game.map.nb, this.game.map.rows);
+        closeCells.forEach(cell=>{ 
+            if( cell.classList.contains("has-player")){
+                this.startCombat();
+            }
+        })
     }
-    const table = document.querySelector('table');
-    let newCell = table.rows[newY].cells[newX];
-    newCell.focus();
+
+    moveFocusedCell(event, startCell, movableCells) {
+
+        const start = document.activeElement;
+        let startX = start.cellIndex;
+	    let startY = start.parentNode.rowIndex;
+
+        console.log(startX, startY);
+        const key = event.key;
+        let newX = startX;
+        let newY = startY;
+        const table = document.querySelector('table');
+
+        console.log(key);
+        if (this.playerTurn) {
+            switch (key) {
+                case "ArrowUp":
+                    if(table.rows[startY - 1].cells[newX] && table.rows[startY - 1].cells[newX].classList.contains('moveCells')){
+                    newY = startY - 1;
+                    }
+                    break;
+                case "ArrowDown":
+                    if(table.rows[startY + 1].cells[newX] && table.rows[startY + 1].cells[newX].classList.contains('moveCells')){
+                    newY = startY + 1;
+                    }
+                    break;
+                case "ArrowLeft":
+                    if(table.rows[newY].cells[startX - 1] && table.rows[newY].cells[startX - 1].classList.contains('moveCells')){
+                    newX = startX - 1;
+                    }
+                    break;
+                case "ArrowRight":
+                    if(table.rows[newY].cells[startX + 1] && table.rows[newY].cells[startX + 1].classList.contains('moveCells')){
+                    newX = startX + 1;
+                    }
+                    break;
+                case "Enter":
+                    this.moveFocusedPlayer(startCell, table.rows[newY].cells[newX], movableCells);
+                    break;
+            }
+            let newCell = table.rows[newY].cells[newX];
+            newCell.focus();
+        }
     }
-  }
 
-  arrowMove(allCells, map){
-    const startCell = allCells.rows[this.curY].cells[this.curX];
-    const movableCells = this.getMovableCells(this.curX, this.curY, map.nb, map.rows);
-    this.showMovableCells(movableCells);
-    startCell.focus();
-    document.addEventListener("keydown", (e) => {
-        e.preventDefault();
-        this.moveFocusedPlayer(e);
-    });
-  }
+    arrowMove(allCells, map) {
 
-
-//  playerPlay(map, allCells) {
-  	// 1 Vérifier si c'est ton tour
-    /*
-    Si false : return;
-    */
-    
-    //2 si ya combattant a cote se battre sinon se deplacer
-    /*
-    Si combattant : Combattre 
-    Sinon : movePlayer    
-    
-    */
-    
-    //3 reinitilaise les tours
-  //}
+        const startCell = allCells.rows[this.curY].cells[this.curX];
+        startCell.classList.add("moveCells");
+        const movableCells = this.getMovableCells(this.curX, this.curY, map.nb, map.rows);
+        this.showMovableCells(movableCells);
+        startCell.focus();
+        document.addEventListener("keydown", (e) => {
+            e.preventDefault();
+            this.moveFocusedCell(e, startCell, movableCells);
+        });
+    }
 
 }
