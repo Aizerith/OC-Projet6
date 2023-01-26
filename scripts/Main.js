e969e1b5b7966693102fce2bedea41c9037d1bf8
Aizerith <101933140+Aizerith@users.noreply.github.com>
2023-01-26 21:56:29 +0100

    Add files via upload

---
+++ scripts/Main.js
@@ -0,0 +1,34 @@
+import { Map } from "../module/Map.js";
+import { Panel } from "../module/Panel.js";
+import { Game } from "../module/Game.js";
+
+// vérification taille d'écran
+let windowSize;
+$(window).on('resize', function(e){
+    windowSize = $(window).width();
+    if(windowSize <= 400){
+        alert("Taille d'écran trop petit pour jouer de manière optimal!");
+    }
+})
+
+// construit la map
+const gameMap = new Map(6, 5);
+gameMap.buildMap();
+gameMap.disableCells();
+
+// créer la classe game qui va créer les joueurs
+const game = new Game(gameMap, "Luke", "Marie");
+
+// créer l'affichage des infos joueur
+const panel1 = new Panel(document.getElementById('displayPlayer1'), game.player1);
+const panel2 = new Panel(document.getElementById('displayPlayer2'), game.player2);
+
+// initialise les parametre des joueurs
+game.initPlayer(panel1, panel2);
+
+// initialise le jeu
+game.initGame();
+
+// affiche les infos joueurs
+panel1.displayPlayerInfo();
+panel2.displayPlayerInfo();
